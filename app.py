83bf50a23023904a5abd2d1f824f69dcac997ff1
clecle <clement.guillemare@insa-lyon.fr>
2020-11-25 00:45:05 +0100

    [bug] impossible de connecter la 2eme enceinte sur le dongle bluetooth si le scan n'est pas active sur cette interface

--- app.py
+++ app.py
@@ -2,13 +2,38 @@ from flask import Flask,request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from bluetool.bluetool import Bluetooth
 import subprocess
+import time
+
+
+
+
+
+
+
+def getBtControllerMacAddr():
+    command='hcitool dev | grep -o \"[[:xdigit:]:]\{11,17\}\"'
+    mac_controller_addr=subprocess.check_output(command, shell=True).decode()
+    controller_list=mac_controller_addr.split('\n')[0:nb_bt_controller]
+    print('my controller_list is ',controller_list[0])
+    return controller_list
+
+#il faudrait pouvoir activer le scan sur la deuxieme interface aussi sinon pas possible de connecter la deuxieme enceinte
+def activateScanForAllBtController():
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,text=True)
+    for i in range(nb_bt_controller):
+        process.stdin.write('select '+controller_list[i]+'\n')
+        time.sleep(1)
+        process.stdin.write('scan on\n')
+        time.sleep(1)
+        process.stdin.write('exit\n')
+ 
 
 app = Flask(__name__)
 FlaskJSON(app)
 bluetooth = Bluetooth()
-adapters  = bluetooth.list_interfaces()
 nb_bt_controller=2
 controller_list=getBtControllerMacAddr()
+#activateScanForAllBtController()
 
 @app.route('/')
 def hello():
@@ -17,7 +42,8 @@ def hello():
 @app.route('/devices/scan')
 @as_json
 def get_devices():
-    bluetooth.scan()
+    #try to activate scan on both controller
+    bluetooth.scan() 
     devices = bluetooth.get_available_devices()
     return devices
 
@@ -54,10 +80,10 @@ def get_connect(mac_addr):
     nb_connectedDevices=len(bluetooth.get_connected_devices())
     print("number of connnected device = ",nb_connectedDevices)
     if nb_connectedDevices==0:
-        connectBluetoothDevice(mac_addr,interface_nb=1)
+        connectBluetoothDevice(mac_addr,interface_nb=0)
         return ("connect.... ok",200)
     elif nb_connectedDevices == 1:
-        connectBluetoothDevice(mac_addr,interface_nb=0)
+        connectBluetoothDevice(mac_addr,interface_nb=1)
         return ("connect.... ok",200)
     else:
         return ("too many bluetooth devices.... failed", 500)
@@ -65,6 +91,8 @@ def get_connect(mac_addr):
 
 ''' def get_connect(mac_addr):
     inArrayC = False
+    print(len(bluetooth.get_connected_devices()))
+    nb_connectedDevices=len(bluetooth.get_connected_devices())
     for i in bluetooth.get_connected_devices():
         print("value: " + str(i.values()))
         if mac_addr.encode() in i.values():
@@ -78,12 +106,12 @@ def get_connect(mac_addr):
     if inArrayC:
         return ("already connected", 200)
     elif inArrayPaired:
-        isConnected = bluetooth.connect(mac_addr)
+        isConnected = bluetooth.connect(mac_addr,adapter_idx=0)
         return ("connect.... ok", 200 if isConnected else 500)
     else:
-        isPaired = bluetooth.pair(mac_addr)
+        isPaired = bluetooth.pair(mac_addr,adapter_idx=0)
         if isPaired:
-            isConnected = bluetooth.connect(mac_addr)
+            isConnected = bluetooth.connect(mac_addr,adapter_idx=0)
             return ("pair.... ok<br>connect.... ok", 200 if isConnected else 500)
         else:
             return ("pair.... failed", 500) '''
@@ -135,26 +163,22 @@ def get_controllers():
 
 
 
-
-def getBtControllerMacAddr():
-    command='hcitool dev | grep -o \"[[:xdigit:]:]\{11,17\}\"'
-    mac_controller_addr=subprocess.check_output(command, shell=True)
-    controller_list=mac_controller_addr.split('\n')[0:nb_bt_controller]
-    print("my controller list is : "+controller_list )
-    return controller_list
-
 def connectBluetoothDevice(mac_addr,interface_nb):
-    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    process.stdin.write('select '+controller_list[interface_nb]+'\n')
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,text=True)
+    process.stdin.write("select "+controller_list[interface_nb]+"\n")
     process.stdin.flush()
+    time.sleep(1)
     process.stdin.write('pair '+ mac_addr+'\n')
     process.stdin.flush()
+    time.sleep(1)
     process.stdin.write('connect '+ mac_addr+'\n')
     process.stdin.flush()
+    time.sleep(2)
     process.stdin.write('exit\n')
     process.stdin.flush()
+    time.sleep(1)
     output, errors = process.communicate()
-    #print(output.decode())
+    print(output,errors)
 
 
 def isDeviceAlreadyConnected(mac_addr):
