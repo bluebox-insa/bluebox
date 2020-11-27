28032554fe58b9b725e4b0e9a86ef412a9e667f1
Gabriel Forien <gforien+dev@gmail.com>
2020-11-27 07:24:15 +0100

    deleted lots of things

--- init.sh
+++ init.sh
@@ -4,6 +4,4 @@
 pactl load-module module-combine-sink sink_name=combined
 pactl set-default-sink combined
 cd ~/innotech-mvp
-IP_ADDR="$(hostname -I | cut -d ' ' -f 1)"
-echo $IP_ADDR
-flask run --host $IP_ADDR
+flask run --host "$(hostname -I | cut -d ' ' -f 1)"
