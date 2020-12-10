b27313ff19b93a233a7c1842ef561a9829b35009
gforien <gforien+dev@gmail.com>
2020-12-10 21:34:26 +0000

    added delays and changed init script

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello():
 @app.route('/scan')
 @as_json
 def scan_for_bluetooth_devices():
-    bluetooth.scan() 
+    bluetooth.scan()
     devices = bluetooth.get_available_devices()
     return devices
 
@@ -40,6 +40,58 @@ def scan_for_bluetooth_devices():
 def get_connected_bluetooth_devices():
     devices = bluetooth.get_connected_devices()
     return devices
+
+@app.route('/connect_input/<mac_addr>')
+@as_json
+def connect_smartphone(mac_addr):
+    if isMacAddrInDevices(mac_addr, bluetooth.get_connected_devices()):
+        return "device already connected", 200
+
+    global controllerIndex, devicesRecord
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
+    logger.debug(f"select controllers[0] = {controllers[0]}\n")
+    process.stdin.write(f"select {controllers[0]}\n")
+    process.stdin.flush()
+
+    if 0 in devicesRecord.keys():
+        logger.debug(f"controllers[{controllerIndex}] is already connected to {devicesRecord[controllerIndex]}")
+        logger.debug(f"disconnect {devicesRecord[controllerIndex]}")
+        process.stdin.write(f"disconnect {devicesRecord[controllerIndex]}")
+        process.stdin.flush()
+        time.sleep(5)
+
+    if not isMacAddrInDevices(mac_addr, bluetooth.get_paired_devices()):
+        process.stdin.write("scan on\n")
+        process.stdin.flush()
+        time.sleep(2)
+
+        logger.debug(f"pair {mac_addr}\n")
+        process.stdin.write(f"pair {mac_addr}\n")
+        process.stdin.flush()
+        time.sleep(7)
+        process.stdin.write(f"yes\n")
+        process.stdin.flush()
+        time.sleep(3)
+    else:
+        logger.debug(f"scan on\n")
+        process.stdin.write(f"scan on\n")
+        process.stdin.flush()
+        time.sleep(4)
+
+    logger.debug(f"connect {mac_addr}\n")
+    process.stdin.write(f"connect {mac_addr}\n")
+    process.stdin.flush()
+    time.sleep(4)
+
+    out, err = process.communicate()
+    print(out)
+
+    if isMacAddrInDevices(mac_addr, bluetooth.get_connected_devices()):
+        devicesRecord[controllerIndex] = mac_addr
+        return "ok", 200
+    else:
+        return "erreur", 500
 
 
 @app.route('/connect/<mac_addr>')
@@ -50,27 +102,37 @@ def connect_bluetooth_device(mac_addr):
     logger.debug(f"select {controllers[currentControllerIndex]}\n")
     process.stdin.write(f"select {controllers[currentControllerIndex]}\n")
     process.stdin.flush()
-    time.sleep(2)
+
+    logger.debug(f"is {controllerIndex} in {devicesRecord.keys()} : {controllerIndex in devicesRecord.keys()}")
+    if controllerIndex in devicesRecord.keys():
+        logger.debug(f"controllers[{controllerIndex}] is already connected to {devicesRecord[controllerIndex]}")
+        logger.debug(f"disconnect {devicesRecord[controllerIndex]}")
+        process.stdin.write(f"disconnect {devicesRecord[controllerIndex]}")
+        process.stdin.flush()
+        time.sleep(5)
+
     process.stdin.write("scan on\n")
     process.stdin.flush()
-    time.sleep(5)
-    process.stdin.write("scan off\n")
-    process.stdin.flush()
-    time.sleep(1)
-    logger.debug(f"pair {mac_addr}\n")
-    process.stdin.write(f"pair {mac_addr}\n")
-    process.stdin.flush()
-    time.sleep(1)
+    time.sleep(3)
+
+    if not isMacAddrInDevices(mac_addr, bluetooth.get_paired_devices()):
+        logger.debug(f"pair {mac_addr}\n")
+        process.stdin.write(f"pair {mac_addr}\n")
+        process.stdin.flush()
+        time.sleep(6)
+
     logger.debug(f"connect {mac_addr}\n")
     process.stdin.write(f"connect {mac_addr}\n")
     process.stdin.flush()
     time.sleep(4)
-    process.stdin.write('exit\n')
-    process.stdin.flush()
-    time.sleep(1)
-    output, errors = process.communicate()
-    print(output,errors)
 
+    if isMacAddrInDevices(mac_addr, bluetooth.get_connected_devices()):
+        devicesRecord[controllerIndex] = mac_addr
+        controllerIndex += 1
+        logger.debug(f"controllerIndex set to {controllerIndex}")
+        return "ok", 200
+    else:
+        return "erreur", 500
 
 @app.route('/play')
 @as_json
@@ -83,17 +145,6 @@ def get_play():
     except Exception as e:
         logger.error(f"Exception at {current_func()}: {e}")
         return f"Exception at {current_func()}: {e}", 500
-
-
-#il faudrait pouvoir activer le scan sur la deuxieme interface aussi sinon pas possible de connecter la deuxieme enceinte
-def activateScanForAllBtController():
-    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,text=True)
-    for i in range(nb_bt_controller):
-        process.stdin.write('select '+controller_list[i]+'\n')
-        time.sleep(1)
-        process.stdin.write('scan on\n')
-        time.sleep(1)
-        process.stdin.write('exit\n')
 
 
 if __name__ == '__main__':
--- init.sh
+++ init.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-#create a simultaneous output
-pactl load-module module-combine-sink sink_name=combined
-pactl set-default-sink combined
-cd ~/innotech-mvp
-flask run --host "$(hostname -I | cut -d ' ' -f 1)"
+set -xo pipefail
+
+# create a simultaneous output
+# pactl cannot be run as root, so we have to do sudo pactl as user pi
+# and define the environment variable XDG_RUNTIME_DIR
+IS_COMBINED_SINK="$(sudo -u pi env XDG_RUNTIME_DIR=/run/user/$(id -u pi) pactl list sinks | grep combined)"
+if [[ -z "$IS_COMBINED_SINK" ]]; then
+    sudo -u pi env XDG_RUNTIME_DIR=/run/user/$(id -u pi) pactl load-module module-combine-sink sink_name=combined
+fi
+sudo -u pi env XDG_RUNTIME_DIR=/run/user/$(id -u pi) pactl set-default-sink combined
+
+# set hci0 Bluetooth interface as sink
+# (!) this command needs to be executed as root
+hciconfig hci0 class 0x200420
+
+# launch BlueBox server
+cd "$(dirname "$0")"
+nohup ./app.py &
+rm nohup.out
+exit
